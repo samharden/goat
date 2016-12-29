861f156a65cc58db290b84aa6e80ef7554b42897
Sam <samharden@gmail.com>
2016-12-29 13:59:50 -0500

    Changes for chapter 5, on to adding second list item

--- lists/tests.py
+++ lists/tests.py
@@ -22,6 +22,14 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
 
         # request = HttpRequest()
         # response = home_page(request)
